fa268bb3ea910465174ac4fff444b54384f32632
rixfm14 <rixfm14@gmail.com>
2024-12-10 22:05:48 +0100

    Added minus sign to correct travel of a wave

--- Lab1_Part2/Plotta_Maxwell_1D.m
+++ Lab1_Part2/Plotta_Maxwell_1D.m
@@ -1,6 +1,6 @@
 % Script that plots the solution from Maxwell_1D_ANM and generates movie
 
-plot(x,V(1:m,old),'r',x,V(m+1:n,old),'b--','LineWidth',1);
+plot(x,V(1:m,old),'r',x,-V(m+1:n,old),'b--','LineWidth',1);
 title(['Numerical solution at t = ',num2str(t)]);
 theAxes=[x_l x_r y_d y_u];
 axis(theAxes);
