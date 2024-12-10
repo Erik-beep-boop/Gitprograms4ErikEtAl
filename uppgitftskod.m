5b3cfa6a42859c7ca7a6fee4f2fc58afd031e329
Filip <rixfm14@gmail.com>
2024-12-10 21:58:59 +0100

    Task 4 kinda done

--- uppgitftskod.m
+++ uppgitftskod.m
@@ -1,10 +1,8 @@
-clear all;
-close all;
-clc;
+clear, clc, close all
 s = settings;
 s.matlab.appearance.figure.GraphicsTheme.TemporaryValue = "light";
 
-ordning=9;
+ordning=6;
 m = 201;
 rho = 1;
 c = 1;
@@ -33,7 +31,7 @@ D_mat =[beta 0
 e1=[1 0];e2=[0 1]; % Pick out variable
 I_2=eye(2);
 
-CFL=0.1; %CFL=k/h
+CFL=0.05; %CFL=k/h
 rr=0.1; %Width of Gaussian
 
 t_1=1.8;
@@ -71,8 +69,8 @@ elseif BC == 2
     L=[kron(e2,e_1');
        kron(e2,e_m')];
 elseif BC == 3
-    L=[kron(e1+beta*e2,e_1');
-       kron(e1-beta*e2,e_m')];
+    L=[kron(e1-beta*e2,e_1');
+       kron(e1+beta*e2,e_m')];
 elseif BC == 4
     L=[kron(e1,e_1');
        kron(e1,e_m')
@@ -82,7 +80,6 @@ end
 
 
 P=I_m2-HI2*L'*inv(L*HI2*L')*L; % Projection operator
-    
 
 T=P*kron(A,D1)*P;
 D=sparse(kron(inv(C),I_m)*T);
@@ -105,20 +102,21 @@ uc1=exp(-((x-t)/rr).^2)';
 uc2=-exp(-((x+t)/rr).^2)';
 
 
-% figure(1);
-% plot(x,uc1,'r',x,uc2,'b');
+%figure(1);
+%plot(x,uc1,'r',x,uc2,'b');
 
-exakt=zeros(n,1);	        % Analytiska l?sningen
-exakt(1:m)=uc2-uc1;
-exakt(m+1:n)=uc2+uc1;
+exakt=zeros(n,1);	        % Analytiska lösningen
+exakt(1:m)=uc1-uc2;
+exakt(m+1:n)=-uc2+uc1;
 
-V=zeros(n,2);	                % L?sningsvektorn
-V(1:m,old)=uc2-uc1;
-V(m+1:n,old)=uc2+uc1;
+V=zeros(n,2);	                % Lösningsvektorn
+V(1:m,old)=uc1-uc2; % pressure
+V(m+1:n,old)=uc2+uc1; % velocity
 felet(1)=sqrt(h)*norm(V(:,old)-exakt);
 
      
 Plotta_Maxwell_1D; % Plott the solution and generate a movie
+
 
 
 for nr_itter=1:max_itter
@@ -169,11 +167,28 @@ end
  end
  
 figure(2);
-plot(x,V(1:m,old),'r',x,V(m+1:n,old),'b--','LineWidth',1);
+plot(x,V(1:m,old),'r',x,-V(m+1:n,old),'b--','LineWidth',1);
 title(['Numerical solution at t = ',num2str(t_1)]);
-axis([x_l x_r -1.1 1.1]);
+axis([x_l x_r y_d y_u]);
 grid;xlabel('x');
-legend('Electric (E)','Magnetic (H)')
+legend('Pressure','Velocity')
+ax = gca;          % current axes
+ax.FontSize = 16;
+
+
+% plot starting values
+t = 0;
+uc1=exp(-((x-t)/rr).^2)';
+uc2=-exp(-((x+t)/rr).^2)';
+V(1:m,old)=uc1-uc2;
+V(m+1:n,old)=uc1+uc2;
+
+figure(3);
+plot(x,V(1:m,old),'r',x,-V(m+1:n,old),'b--','LineWidth',1);
+title(['Numerical solution at t = ',num2str(t)]);
+axis([x_l x_r y_d y_u]);
+grid;xlabel('x');
+legend('Pressure','Velocity')
 ax = gca;          % current axes
 ax.FontSize = 16;
 
